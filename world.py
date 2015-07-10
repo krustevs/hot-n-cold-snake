5793f6440ee94c153726ce10cdeb80ba60f92729
simo <simeon.krustev@gmail.com>
2015-07-11 02:48:14 +0300

    implemented tail bite

--- world.py
+++ world.py
@@ -1,8 +1,11 @@
 from mcpi import minecraft
 from mcpi.vec3 import Vec3
 from math import floor
+from itertools import product
 
 from snake import Snake
+
+shell = [Vec3(i[0], i[1], i[2]) for i in list(product([-1, 0, 1], [-1, 0, 1], [-1, 0, 1]))]
 
 class World():
 
@@ -12,7 +15,7 @@ class World():
         self.snake = Snake(self.mc)
 
     def get_player_pos(self):
-        return self.mc.player.getPos()
+        return self.mc.player.getTilePos()
 
     def get_apple_pos(self):
         return self.apple
@@ -31,9 +34,13 @@ class World():
         self.apple = None
 
     def check_collision(self):
-        collision = self.get_player_pos()# + self.mc.player.getDirection()
-        pos = [floor(axis) for axis in collision]
-        return (pos[0], pos[1], pos[2]) in self.snake.body
+        pos = [floor(axis) for axis in self.get_player_pos()]
+        center = Vec3(pos[0], pos[1], pos[2])
+        for offset in shell:
+            block = center + offset
+            if (block.x, block.y, block.z) in list(self.snake.body)[3:]:
+                return True
+        return False
 
     def move_snake(self):
         new_pos = self.get_player_pos()
